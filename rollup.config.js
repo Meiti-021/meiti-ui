3823e87b99b5cd7f6e4217010de77fc47b20c9ec
Mahdi dehgani <89641841+Meiti-021@users.noreply.github.com>
2025-05-10 11:56:52 +0330

    initial commit
    An initial commit for basic npm package repository(Test case)

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: "dist/index.es.js",
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    postcss(),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfig: "./tsconfig.json",
+      clean: true
+    })
+  ]
+};
